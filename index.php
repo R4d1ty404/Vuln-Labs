1beceb92904cb650969118f8d8c962fb94a002f9
R4d1ty404 <152013821+R4d1ty404@users.noreply.github.com>
2024-08-09 22:02:06 +0700

    Home

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>XSS LABS</title>
+    </head>
+    <body>
+        <h1>Welcome to Vuln-Labs</h1>
+        <p>Made By Raditya Perwira Putra</p>
+        <h4>Below are the avaible vulnerability labs</h4>
+        <a href="xss_labs.php">XSS LABS</a>
+        <br>
+        <a href="sqli.php">SQLI LABS</a>
+    </body>
+</html>
